78df5798f4c9642f4cf32ccb5ccfe962b14fdf9b
Magda <hromagda@centrum.cz>
2025-05-21 20:20:04 +0200

    Vytvorena Administratorska sekce s editaci textu v akcnich nabidkach, galerie, clanku a zobrazeni vsech zprav z mailformu

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'petrk/alukol-web',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7b63052a3ec8ac36305a59175c4be58cae1aba5f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'petrk/alukol-web' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7b63052a3ec8ac36305a59175c4be58cae1aba5f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
